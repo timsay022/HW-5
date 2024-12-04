142f94d10efef248b552dffc49db39d56e6b6fcb
timsay022 <saifitdinov.t@yandex.ru>
2024-12-04 23:49:48 +0500

    added EnterprisePageLoadTest and DragAndDropTest

--- src/test/java/github/EnterprisePageLoadTest.java
+++ src/test/java/github/EnterprisePageLoadTest.java
@@ -1,4 +1,19 @@
 package github;
 
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class EnterprisePageLoadTest {
+
+    @Test
+    void openEnterprisePage() {
+        open("https://github.com/");
+        $(".HeaderMenu-nav").$(byTagAndText("button", "Solutions")).hover();
+        $(".HeaderMenu-nav").$(byTagAndText("a", "Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+
+    }
 }
--- src/test/java/herokuapp/DragAndDropTest.java
+++ src/test/java/herokuapp/DragAndDropTest.java
@@ -1,4 +1,37 @@
 package herokuapp;
 
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class DragAndDropTest {
+    @BeforeAll
+    static void setup() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+    }
+
+    @Test
+    void dragAndDropWithActions() {
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().dragAndDrop($("#column-a"), $("#column-b")).build().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+
+    @Test
+    void dragAndDrop() {
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        sleep(5000);
+
+    }
 }
